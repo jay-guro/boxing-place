66a81dc6a57a3c3548292b0a1df32c0f2ff41bd9
Jason Guro <62923544+jay-guro@users.noreply.github.com>
2021-05-20 18:43:02 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@
   
 /* 1. Proloder */
     $(window).on('load', function () {
-      $('#preloader-active').delay(450).fadeOut('slow');
-      $('body').delay(450).css({
+      $('#preloader-active').delay(1000).fadeOut('slow');
+      $('body').delay(1000).css({
         'overflow': 'visible'
       });
     });
